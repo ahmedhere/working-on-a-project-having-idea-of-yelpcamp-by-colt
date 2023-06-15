21964657af395278bcc90bcc3e483430fd370c71
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-15 23:41:57 +0500

    mongoose is connected with the mongodb

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/champground");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello from yelpcamp");
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "Cheap Camping",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
