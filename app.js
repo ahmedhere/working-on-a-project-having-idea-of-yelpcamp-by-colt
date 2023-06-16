11191b43b0f4d9e2c7c4ee568c43b260d691d62d
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-16 13:55:16 +0500

    EJS MATE introduction

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const path = require("path");
 const methodOverRide = require("method-override");
+const ejsMate = require("ejs-mate");
 const routesCampGround = require("./Routes/campground");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${new Date().toLocaleString()} ${req.method} ${req.path}`);
+  next();
+});
 app.use(methodOverRide("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 app.use("/", routesCampGround);
 
