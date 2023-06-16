1842cb13ae3af56242ed02d498f484b6de67117a
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-16 10:01:02 +0500

    Edit page and update functionality added

--- Routes/campground.js
+++ Routes/campground.js
@@ -18,10 +18,34 @@ Router.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+Router.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
 Router.get("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render("campgrounds/show", { campground });
 });
 
+Router.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`);
+});
+
+Router.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+Router.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground,
+    { new: true }
+  );
+  res.render("campgrounds/show", { campground });
+});
 module.exports = Router;
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const methodOverRide = require("method-override");
 const routesCampGround = require("./Routes/campground");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverRide("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
