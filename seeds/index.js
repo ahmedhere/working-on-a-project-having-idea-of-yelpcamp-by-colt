9fa8ede42f9b676c45bb9198569d049c5f662ef2
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-16 15:09:04 +0500

    Styling is not added. Data is populated from the db

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, molestiae labore, saepe commodi nisi expedita voluptatem sint minima ad libero iure voluptates veritatis, repellat tempore odio alias deserunt vitae. Impedit! Sit debitis, unde ipsam voluptas laboriosam nisi expedita iste ut magnam et. Molestiae accusantium itaque voluptates deserunt minima blanditiis molestias debitis vero odit. Magni alias provident optio explicabo commodi soluta!`,
+      price,
     });
     await camp.save();
   }
