be990e33b7c6d669c24e5f8e8c6feb0865f7f80e
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-17 08:21:21 +0500

    Added error handling, form validation, created request logs and error logs on the time of request with the timestamp

--- Routes/campground.js
+++ Routes/campground.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Campground = require("../models/champground");
+const AppError = require("../AppError");
 
 const Router = express.Router();
 
@@ -21,31 +22,68 @@ Router.get("/campgrounds", async (req, res) => {
 Router.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
-Router.get("/campgrounds/:id", async (req, res) => {
+Router.get("/campgrounds/:id", async (req, res, next) => {
   const { id } = req.params;
+  if (id < 24 || id > 24) {
+    return next(
+      new AppError(
+        401,
+        "Invalid ID. The id you entered doesn't match the legacy"
+      )
+    );
+  }
+
   const campground = await Campground.findById(id);
+  if (!campground) {
+    return next(new AppError(404, "not found"));
+  }
   res.render("campgrounds/show", { campground });
 });
 
-Router.post("/campgrounds", async (req, res) => {
+Router.post("/campgrounds", async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
+  if (!campground.id) {
+    return next(new AppError(404, "Not Found"));
+  }
   res.redirect(`campgrounds/${campground._id}`);
 });
 
 Router.get("/campgrounds/:id/edit", async (req, res) => {
   const { id } = req.params;
+  if (id < 24 || id > 24) {
+    return next(
+      new AppError(
+        401,
+        "Invalid ID. The id you entered doesn't match the legacy"
+      )
+    );
+  }
   const campground = await Campground.findById(id);
+  if (!campground) {
+    return next(new AppError(404, "not found"));
+  }
   res.render("campgrounds/edit", { campground });
 });
 
 Router.put("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
+  if (id < 24 || id > 24) {
+    return next(
+      new AppError(
+        401,
+        "Invalid ID. The id you entered doesn't match the legacy"
+      )
+    );
+  }
   const campground = await Campground.findByIdAndUpdate(
     id,
     req.body.campground,
     { new: true }
   );
+  if (!campground) {
+    return next(new AppError(404, "not found"));
+  }
   res.render("campgrounds/show", { campground });
 });
 
--- app.js
+++ app.js
@@ -2,22 +2,50 @@ const express = require("express");
 const path = require("path");
 const methodOverRide = require("method-override");
 const ejsMate = require("ejs-mate");
+const fs = require("fs");
 const routesCampGround = require("./Routes/campground");
+const AppError = require("./AppError");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverRide("_method"));
 app.use((req, res, next) => {
-  console.log(`${new Date().toLocaleString()} ${req.method} ${req.path}`);
+  const request = `${new Date().toLocaleString()} ${req.method} ${req.url}\n`;
+  fs.appendFile("request.log", request, (error) => {
+    if (error) {
+      console.log("Typing error");
+    } else {
+      console.log("request logged");
+    }
+  });
   next();
 });
-app.use(methodOverRide("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
 
 app.use("/", routesCampGround);
+
+app.use("*", (req, res) => {
+  res.status(404).send("not found");
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  const logerror = `${new Date().toLocaleString()} - ${req.method} - ${
+    req.url
+  } - ${status} - ${message} \n `;
+  fs.appendFile("error.log", logerror, (error) => {
+    if (error) {
+      console.log("logging error");
+    } else {
+      console.log("error has been logged and handled");
+    }
+  });
+  res.status(status).send(message);
+});
 
 app.listen(3000, () => {
   console.log("Yup, We are listening at the port number 3000");
