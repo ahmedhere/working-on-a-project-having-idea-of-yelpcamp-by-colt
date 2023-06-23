f1e53fbc4ebbe6d9a6f62555ee9dc83dcdb20ead
ahmedhere <104449961+ahmedhere@users.noreply.github.com>
2023-06-23 18:45:55 +0500

    Update the authorization function into seperate middleware and validation functions tooo

--- Routes/campground.js
+++ Routes/campground.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Campground = require("../models/champground");
 const AppError = require("../AppError");
-const { isLoggedIn } = require("../middleware/isloggedin");
-const { campGroundSchema } = require("../Schemas.js");
+const {
+  isLoggedIn,
+  validateCampground,
+  isAuthor,
+} = require("../middleware/isloggedin");
 
 const Router = express.Router();
 mongoose
@@ -14,16 +17,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-const validateCampground = (req, res, next) => {
-  const { error } = campGroundSchema.validate(req.body);
-  if (error) {
-    const message = error.details.map((el) => el.message).join(",");
-    throw new AppError(400, message);
-  } else {
-    next();
-  }
-};
 
 Router.get("/", async (req, res, next) => {
   try {
@@ -79,7 +72,7 @@ Router.post("/", isLoggedIn, validateCampground, async (req, res, next) => {
   }
 });
 
-Router.get("/:id/edit", isLoggedIn, async (req, res, next) => {
+Router.get("/:id/edit", isLoggedIn, isAuthor, async (req, res, next) => {
   try {
     const { id } = req.params;
     if (id.length < 24 || id.length > 24) {
@@ -91,10 +84,6 @@ Router.get("/:id/edit", isLoggedIn, async (req, res, next) => {
       );
     }
     const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-      req.flash("error", "You do not have permission to edit it!");
-      return res.redirect(`/campgrounds/${id}`);
-    }
     if (!campground) {
       req.flash("error", "cannot find the campground");
       return res.redirect("/campgrounds");
@@ -106,38 +95,40 @@ Router.get("/:id/edit", isLoggedIn, async (req, res, next) => {
   }
 });
 
-Router.put("/:id", isLoggedIn, validateCampground, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    if (id.length < 24 || id.length > 24) {
-      return next(
-        new AppError(
-          401,
-          "Invalid ID. The id you entered doesn't match the legacy"
-        )
-      );
-    }
-    const campFound = await Campground.findById(id);
-    if (!campFound.author.equals(req.user._id)) {
-      req.flash("error", "You do not have permission to edit it!");
-      return res.redirect(`/campgrounds/${id}`);
-    }
-    const campground = await Campground.findByIdAndUpdate(
-      id,
-      req.body.campground,
-      { new: true }
-    );
-    if (!campground) {
-      return next(new AppError(404, "not found"));
-    }
-    req.flash("success", "Successfully updated");
-    res.redirect(`/campgrounds/${campground._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+Router.put(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  validateCampground,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      if (id.length < 24 || id.length > 24) {
+        return next(
+          new AppError(
+            401,
+            "Invalid ID. The id you entered doesn't match the legacy"
+          )
+        );
+      }
 
-Router.delete("/:id", isLoggedIn, async (req, res, next) => {
+      const campground = await Campground.findByIdAndUpdate(
+        id,
+        req.body.campground,
+        { new: true }
+      );
+      if (!campground) {
+        return next(new AppError(404, "not found"));
+      }
+      req.flash("success", "Successfully updated");
+      res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+Router.delete("/:id", isLoggedIn, isAuthor, async (req, res, next) => {
   try {
     const { id } = req.params;
     if (id.length < 24 || id.length > 24) {
@@ -148,11 +139,7 @@ Router.delete("/:id", isLoggedIn, async (req, res, next) => {
         )
       );
     }
-    const campFound = await Campground.findById(id);
-    if (!campFound.author.equals(req.user._id)) {
-      req.flash("error", "You do not have permission to edit it!");
-      return res.redirect(`/campgrounds/${id}`);
-    }
+
     await Campground.findByIdAndDelete(id);
     req.flash("success", "Successfully deleted a campground");
     res.redirect("/campgrounds");
--- middleware/isloggedin.js
+++ middleware/isloggedin.js
@@ -1,3 +1,5 @@
+const Campground = require("../models/champground");
+const { campGroundSchema } = require("../Schemas.js");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -11,6 +13,26 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.storeReturnTo = (req, res, next) => {
   if (req.session.returnTo) {
     res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campGroundSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new AppError(400, message);
+  } else {
+    next();
+  }
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campFound = await Campground.findById(id);
+  if (!campFound.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to edit it!");
+    return res.redirect(`/campgrounds/${id}`);
   }
   next();
 };
